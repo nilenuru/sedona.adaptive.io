177741648dd09cf7f8acb754b2bcd9f54f4aa57d
alex <zagoraevav@gmail.com>
2019-04-09 09:02:17 +0300

    2

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,40 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    gulp = require('gulp'),
-    concatCss = require('gulp-concat')
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  concatCss = require('gulp-concat'),
+  cleanCSS = require('gulp-clean-css'),
+  rename = require("gulp-rename"),
+  //postcss = require('gulp-postcss'),
+  watch = require('gulp-watch');
 
-gulp.task('default', function() {
+  gulp.task('sass', function(){
+      return gulp.src('source/sass/**/*.scss')
+          .pipe(sass())
+          .pipe(gulp.dest('source/css'))
+          .pipe(browserSync.reload({stream: true}))
+  });
+
+  gulp.task('browser-sync', function() {
+      browserSync({
+          server: {
+              baseDir: 'source'
+          },
+          notify: false
+      });
+  });
+
+  gulp.task('default', function() {
     return gulp.src('source/css/*.css')
-        .pipe(concatCss('styles/style111.css'))
-        .pipe(gulp.dest('source/css'))
-});
+      .pipe(concatCss('style.css'))
+      .pipe(cleanCSS({
+        compatibility: 'ie8'
+      }))
+      //  .pipe(postcss())
+      .pipe(rename('style_min.css'))
+      .pipe(gulp.dest('source/css'))
+  });
+
+  gulp.task('watch', function() {
+      gulp.watch('source/sass/**/*.scss', gulp.parallel('sass'));
+  });
+  gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
